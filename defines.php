f8dcf253aed24afac457b812870fb9b14e6bd8ec
Robert Smith <bobbyx140x@gmail.com>
2017-09-26 21:59:38 -0400

    still debuggin db

--- defines.php
+++ defines.php
@@ -5,4 +5,4 @@ define("HOME_URL", HEROKU_URL . "/src/home.php");
 
 define("DB_USERNAME", "bf8fd052104f8d");
 define("DB_PASSWORD", "9d0ac3f3");
-define("DB_URL", "bf8fd052104f8d:9d0ac3f3@us-cdbr-iron-east-05.cleardb.net");
+define("DB_URL", "us-cdbr-iron-east-05.cleardb.net");
