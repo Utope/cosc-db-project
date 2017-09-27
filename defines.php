3734496c267cc62652003f224b95ca1e890ffb87
Robert Smith <bobbyx140x@gmail.com>
2017-09-26 21:44:20 -0400

    Added database connection

--- defines.php
+++ defines.php
@@ -2,3 +2,7 @@
 
 define("HEROKU_URL", "https://cosc-db-project.herokuapp.com");
 define("HOME_URL", HEROKU_URL . "/src/home.php");
+
+define("DB_USERNAME", "bf8fd052104f8d");
+define("DB_PASSWORD", "9d0ac3f3");
+define("DB_URL", "mysql://bf8fd052104f8d:9d0ac3f3@us-cdbr-iron-east-05.cleardb.net/heroku_02a3be49d2a30a0?reconnect=true");
--- src/home.php
+++ src/home.php
@@ -1,3 +1,14 @@
 <?php
+
+require_once 'defines.php';
+
+$conn = new mysqli(DB_URL,DB_USERNAME,DB_PASSWORD);
+
+if($conn->connect_error){
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+var_dump($conn);
+
 echo "Home page <br>";
 echo "blahhhh";
