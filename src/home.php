789a37416197f1be255b5c6db6c7971da95cc8bb
Robert Smith <bobbyx140x@gmail.com>
2017-09-26 21:54:33 -0400

    debugging database still

--- src/home.php
+++ src/home.php
@@ -1,12 +1,11 @@
 <?php
 
-require_once 'defines.php';
+require_once '../defines.php';
 
-$conn = new mysqli(DB_URL,DB_USERNAME,DB_PASSWORD);
+$conn = mysqli_connect(DB_URL,DB_USERNAME,DB_PASSWORD);
 
-if($conn->connect_error){
-    echo $conn->connect_error;
-    die("Database connection failed: " . $conn->connect_error);
+if(!$conn){
+    die("Connection Failed: " . mysqli_connect_error());
 }
 
 
