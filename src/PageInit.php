aa430b08c61e78c5ae98022d7e2c2347dd29ccf5
Robert Smith <bobbyx140x@gmail.com>
2017-09-28 14:24:02 -0400

    looking at server vars

--- src/PageInit.php
+++ src/PageInit.php
@@ -5,4 +5,5 @@ session_start();
 if($_SESSION['loggedIn'] != true){
  //header("Location: " . "http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]); 
   echo "http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+  var_dump($_SERVER);
 }
