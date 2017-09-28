e2266f6b1aca9ec359b44059594eb59fb354898f
Robert Smith <bobbyx140x@gmail.com>
2017-09-28 14:01:58 -0400

    testing session/login

---
+++ src/PageInit.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+
+//If user is not logged in redirect them to login
+if($_SESSION['loggedIn'] != true){
+ //header("Location: " . "http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]); 
+  echo "http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+}
