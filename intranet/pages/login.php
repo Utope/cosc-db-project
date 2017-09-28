385177411b834ad31769bbd32955d6c428fc3b17
Robert Smith <bobbyx140x@gmail.com>
2017-09-28 14:53:04 -0400

    added header to login

--- intranet/pages/login.php
+++ intranet/pages/login.php
@@ -3,19 +3,23 @@
 require_once 'defines.php';
 ?>
 
-<head>
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
-</head>
-
-<body>
-	<form class="w3-container">
-		<label>Login</label>
-		
-		<label>username</label>
-		<input class="w3-input" type="text">
-		
-		<label>password</label>
-		<input class="w3-input" type="text">
-	</form>	
-
-</body>
+<!DOCTYPE html>
+<html>
+    <head>
+    	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    	<title>login</title>
+    </head>
+    
+    <body>
+    	<form class="w3-container">
+    		<label>Login</label>
+    		
+    		<label>username</label>
+    		<input class="w3-input" type="text">
+    		
+    		<label>password</label>
+    		<input class="w3-input" type="text">
+    	</form>	
+    
+    </body>
+</html>
