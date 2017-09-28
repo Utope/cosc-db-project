c839eb5cb4718b9d287eafb6e4d6626de99e8210
Robert Smith <bobbyx140x@gmail.com>
2017-09-28 14:49:37 -0400

    login graphics

--- intranet/pages/login.php
+++ intranet/pages/login.php
@@ -1,2 +1,21 @@
+
 <?php
-echo "Your are at in the login page!";
+require_once 'defines.php';
+?>
+
+<head>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+</head>
+
+<body>
+	<form class="w3-container">
+		<label>Login</label>
+		
+		<label>username</label>
+		<input class="w3-input" type="text">
+		
+		<label>password</label>
+		<input class="w3-input" type="text">
+	</form>	
+
+</body>
